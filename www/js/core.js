5736b49751e0d835edbb1486700fa3674e2da5b9
Kirk Portas <portk@kkr.com>
2016-06-09 10:51:42 -0400

    WIP pre-partial/hbs

--- www/js/core.js
+++ www/js/core.js
@@ -1,44 +1,3 @@
-var PhoneGapProxy = {
-    'navigator': {
-        'notification': {
-            'vibrate': function (a) {
-                if (navigator.notification && navigator.notification.vibrate) {
-                    navigator.notification.vibrate(a);
-                } else {
-                    console.warn('navigator.notification.vibrate');
-                }
-            },
-            'alert': function (a, b, c, d) {
-                if (navigator.notification && navigator.notification.alert) {
-                    navigator.notification.alert(a, b, c, d);
-                } else {
-                    console.warn('navigator.notification.alert');
-                    alert(a);
-                }
-            },
-            'confirm': function (message, callback, title, labels) {
-                if (navigator.notification && navigator.notification.confirm) {
-                    navigator.notification.confirm(message, callback, title, labels);
-                } else {
-                    console.warn('navigator.notification.alert');
-                    if (confirm(message)) {
-                        auth.handleLogout();
-                    }
-                }
-            }
-        },
-        'splashscreen': {
-            'hide': function () {
-                if (navigator.splashscreen) {
-                    navigator.splashscreen.hide();
-                } else {
-                    console.warn('navigator.splashscreen.hide');
-                }
-            }
-        }
-    }
-};
-
 if (!String.prototype.startsWith) {
     Object.defineProperty(String.prototype, 'startsWith', {
         enumerable: false,
@@ -51,20 +10,6 @@ if (!String.prototype.startsWith) {
     });
 }
 
-Handlebars.registerHelper('humanizeInches', function(value) {
-    var threshold = 36;  // Convert everything bigger than 36 inches to feet
-
-    if (value >= threshold) {
-        var feet = Math.floor(value / 12);
-        var inches = value % 12;
-        var output = feet + '\' ';
-        if (inches > 0) output += inches + '"';
-        return output;
-    } else {
-        return value + '"';
-    }
-});
-
 Handlebars.registerHelper('ifMore', function(v1, v2, options) {
     if(v1 > v2) {
         return options.fn(this);
@@ -75,85 +20,15 @@ Handlebars.registerHelper('ifMore', function(v1, v2, options) {
 var core = {
     DEBUG: false,
     pageHistoryCount: 0,
-    goingBack: false,
 
-    fixPaddingForIOS7: function () {
-        if (window.device) {
-            if (parseFloat(window.device.version) >= 7.0) {
-                if (core.DEBUG) { console.info('Fixed top padding for iOS 7'); }
-                $('.header_override').css({'padding-top': '20px'});
-            }
-        }
-    },
-    patchImagesWithLettersForGrid: function (images) {
-        var letters = ['a', 'b', 'c'];
-        var innerCounter = 0;  // We use it for counting 1-3, letters a, b, c)
+    // confirmExit: function () {
+    //     navigator.notification.confirm("Are you sure you want to exit?", function(result){
+    //         if (result == 2) {
+    //             core.exitApp();
+    //         }
+    //     }, 'Exit', 'No,Exit');
+    // },
 
-        if (!images.length) return [];
-
-        for (var i = 0; i < images.length; i++) {
-            if (innerCounter == 3) innerCounter = 0;  // Time to reset
-            images[i].letter = letters[innerCounter];
-            innerCounter += 1;
-        }
-
-        return images;
-    },
-    // ANDROID ONLY
-    exitApp: function () {
-        if (core.DEBUG) { console.log("Exiting app"); }
-        navigator.app.exitApp();
-    },
-    confirmExit: function () {
-        navigator.notification.confirm("Are you sure you want to exit?", function(result){
-            if (result == 2) {
-                core.exitApp();
-            } 
-        }, 'Exit', 'No,Exit');
-    },
-    // Handle the back button
-    onBackKeyDown: function (e) {
-        e.preventDefault();
-
-        var page = $(':mobile-pagecontainer').pagecontainer('getActivePage');
-
-        //alert(page.attr('id'));
-        
-        if (page.attr('id') == 'inventoryListPage') {
-            app.showRequestsListView();
-            core.goingBack = true;
-            core.pageHistoryCount--;
-        } else if (page.attr('id').startsWith('inventoryDetailPage')) {
-            app.showInventoryListView();
-            core.goingBack = true;
-            core.pageHistoryCount--;
-        } else if (page.attr('id') == 'requestListPage') {
-            app.showInventoryListView();
-            core.goingBack = true;
-            core.pageHistoryCount--;
-        } else if (page.attr('id').startsWith('requestDetailPage')) {
-            app.showRequestsListView();
-            core.goingBack = true;
-            core.pageHistoryCount--;
-        } else if (page.attr('id') == 'contactPage') {
-            requests.showRequestDetailsView(inventory.current_item_id);
-            //app.showRequestsListView();
-            core.goingBack = true;
-            core.pageHistoryCount--;
-        } else if (page.attr('id') == 'loginPage') {
-            core.confirmExit();
-        } else if (page.attr('id') == 'noAccountPage') {
-            app.showLoginView();
-            core.pageHistoryCount = 0;
-        } else if (page.attr('id') == 'capturePage') {
-            inventory.showItemsDetailsView(inventory.current_item_id);
-            //app.showInventoryListView();
-            core.goingBack = true;
-            core.pageHistoryCount--;
-        } else {
-            core.confirmExit();
-        }
-    },
     isAndroid: function () {
         return window.device && window.device.platform == 'Android';
     },
@@ -190,14 +65,5 @@ var core = {
     },
     alreadyRendered: function (id) {
         return $('body > div#' + id).length;
-    },
-    showPage: function (id) {
-        $(':mobile-pagecontainer').pagecontainer('change', '#' + id);
-    },
-    setActiveMenuItem: function (name) {
-        $.mobile.activePage.find('a[data-icon="' + name + '"]').addClass('ui-btn-active');
-    },
-    removePage: function (pageId) {
-        $('body > div[id="' + pageId + '"]').remove();
     }
 };
